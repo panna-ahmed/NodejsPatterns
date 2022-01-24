9c373fb6d8716c084bc22c77cd39e44dfd546f5c
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-01-24 14:27:43 +0600

    Chain of Responsility

--- Behavioral Patterns/Chain Of Responsibility/index.js
+++ Behavioral Patterns/Chain Of Responsibility/index.js
@@ -1,3 +1,6 @@
+// Identification: The pattern is recognizable by behavioral methods of one group of objects that indirectly call
+// the same methods in other objects, while all the objects follow the common interface.
+
 var Request = function (amount) {
   this.amount = amount;
 };
